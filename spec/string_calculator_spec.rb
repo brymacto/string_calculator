349632061e022b10cfa3a3e0b6e6b70326cd4827
Bryan MacDiarmid <bryanm@nulogy.com>
2015-10-28 13:41:19 -0400

    Refactored

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,23 +3,23 @@ require_relative '../string_calculator'
 describe Calculator do
   describe "#add" do
     it "returns zero for empty string" do
-      expect(Calculator.add('')).to eql(0)
+      expect(subject.add('')).to eql(0)
     end
 
     it "returns sum given numbers" do
-      expect(Calculator.add('1')).to eql(1)
-      expect(Calculator.add('1,2')).to eql(3)
-      expect(Calculator.add("1,2,3,4")).to eql(10)
+      expect(subject.add('1')).to eql(1)
+      expect(subject.add('1,2')).to eql(3)
+      expect(subject.add("1,2,3,4")).to eql(10)
     end
 
     it "supports new line or comma as separator" do
-      expect(Calculator.add("1,2\n3")).to eql(6)
+      expect(subject.add("1,2\n3")).to eql(6)
     end
 
     it "ignores numbers bigger than one thousand" do
-      expect(Calculator.add("1,999,3")).to eql(1003)
-      expect(Calculator.add("1,1000,3")).to eql(1004)
-      expect(Calculator.add("1,1001,3")).to eql(4)
+      expect(subject.add("1,999,3")).to eql(1003)
+      expect(subject.add("1,1000,3")).to eql(1004)
+      expect(subject.add("1,1001,3")).to eql(4)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,6 @@
-module Calculator
+class Calculator
 
-  def self.add(numbers)
+  def add(numbers)
     numbers.split(/[,\n]/).map(&:to_i).reject{ |num| num > 1000 }.reduce(0, :+)
   end
 end
