2137dbd48a998ae9680ad1c73fc760c41dad6941
Bryan MacDiarmid <bryanm@nulogy.com>
2015-10-28 12:28:03 -0400

    refactored tests

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,45 +3,23 @@ require_relative '../string_calculator'
 describe Calculator do
   describe "#add" do
     it "returns zero for empty string" do
-      sum = Calculator.add('')
-
-      expect(sum).to eql(0)
+      expect(Calculator.add('')).to eql(0)
     end
 
-    it "returns sum given single number" do
-      sum = Calculator.add('1')
-
-      expect(sum).to eql(1)
+    it "returns sum given numbers" do
+      expect(Calculator.add('1')).to eql(1)
+      expect(Calculator.add('1,2')).to eql(3)
+      expect(Calculator.add("1,2,3,4")).to eql(10)
     end
 
-    it "returns sum given two numbers" do
-      sum = Calculator.add('1,2')
-      sum2 = Calculator.add('2,2')
-
-      expect(sum).to eql(3)
-      expect(sum2).to eql(4)
-    end
-
-    it "returns sum given four numbers" do
-      sum = Calculator.add("1,2,3,4")
-
-      expect(sum).to eql(10)
-    end
-
-    it "returns sum given string containing comma and new line as separators" do
-      sum = Calculator.add("1,2\n3")
-
-      expect(sum).to eql(6)
+    it "supports new line or comma as separator" do
+      expect(Calculator.add("1,2\n3")).to eql(6)
     end
 
     it "ignores numbers bigger than one thousand" do
-      sum1 = Calculator.add("1,999,3")
-      sum2 = Calculator.add("1,1000,3")
-      sum3 = Calculator.add("1,1001,3")
-
-      expect(sum1).to eql(1003)
-      expect(sum2).to eql(1004)
-      expect(sum3).to eql(4)
+      expect(Calculator.add("1,999,3")).to eql(1003)
+      expect(Calculator.add("1,1000,3")).to eql(1004)
+      expect(Calculator.add("1,1001,3")).to eql(4)
     end
   end
 end
