9d7e395cc322f83a10790c796ff63753ef862073
Bryan MacDiarmid <bryanm@nulogy.com>
2015-10-28 11:45:25 -0400

    Refactored add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,6 @@
 module Calculator
 
   def self.add(numbers)
-    num_array = numbers.split(",")
-    num_array.map! { |num| num.to_i }
-    num_array.reduce(0,:+)
+    numbers.split(",").map(&:to_i).reduce(0, :+)
   end
 end
