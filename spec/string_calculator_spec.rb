d401329bd356adb05dfe88d2bac5394f3b004ace
Bryan MacDiarmid <bryanm@nulogy.com>
2015-10-28 11:52:52 -0400

    Test for four numbers now passes

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,7 +8,7 @@ describe Calculator do
       expect(sum).to eql(0)
     end
 
-    it "returns sum given number" do
+    it "returns sum given single number" do
       sum = Calculator.add('1')
 
       expect(sum).to eql(1)
@@ -20,6 +20,12 @@ describe Calculator do
 
       expect(sum).to eql(3)
       expect(sum2).to eql(4)
+    end
+
+    it "returns sum given four numbers" do
+      sum = Calculator.add("1,2,3,4")
+
+      expect(sum).to eql(10)
     end
   end
 end
